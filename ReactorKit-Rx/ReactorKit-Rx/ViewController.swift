63206e2a59619378746966b5856a96d21f97ce1b
motosw3600 <motosw3600@gmail.com>
2022-02-13 22:15:25 +0900

    layout: setup layout

--- ReactorKit-Rx/ReactorKit-Rx/ViewController.swift
+++ ReactorKit-Rx/ReactorKit-Rx/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loadingLabel: UILabel!
+    @IBOutlet weak var loadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
