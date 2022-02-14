3fa086796182ca813cbece0d104fe499880c9ce9
motosw3600 <motosw3600@gmail.com>
2022-02-14 15:50:19 +0900

    feat: ReactorKit setup

--- ReactorKit-Rx/ReactorKit-Rx/ViewController.swift
+++ ReactorKit-Rx/ReactorKit-Rx/ViewController.swift
@@ -6,16 +6,39 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import ReactorKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, StoryboardView {
 
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var loadingLabel: UILabel!
+    @IBOutlet weak var imageView: CustomImageView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     @IBOutlet weak var loadButton: UIButton!
+    
+    var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.parentVC = self
+    }
+    
+    func bind(reactor: DefaultReactor) {
+        loadButton.rx.tap
+            .map { Reactor.Action.imageLoadButtonTap(index: Int.random(in: 0...2 )) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.image ?? "" }
+            .distinctUntilChanged()
+            .subscribe(onNext: { self.imageView.loadImage(urlString: $0) })
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.isLoading }
+            .bind(to: indicator.rx.isAnimating)
+            .disposed(by: disposeBag)
     }
 
 
